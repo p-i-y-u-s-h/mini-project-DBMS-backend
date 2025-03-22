f97bd6285e0dd12f84b1cb3df9aec2247a4cfd14
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-23 00:04:13 +0530

    updated

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { genrateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async (req,res)=>{
     const {fullName,email,password} = req.body
@@ -84,5 +85,33 @@ export const logout = (req,res)=>{
     } catch (error) {
         console.log("Error in logout controller",error.message);
         res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const updateProfile = async (req,res)=>{
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile pic is required"});
+        }
+
+        const uplodResponse = await cloudinary.uploader(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId,{profilepic:uplodResponse.secure_url},{new:true})
+
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message:"Internal server error"}) 
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message:"Internal server error"}) 
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRouter from "./routes/auth.route.js";
@@ -9,7 +10,9 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.use(express.json()); 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth",authRouter)
 
 app.listen(PORT,()=>{
